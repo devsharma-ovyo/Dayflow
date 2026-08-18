d5da9612e76fc800d8c63b6ca803666f4ee4cbdd
Dev Sharma <106147268+devsharma-ovyo@users.noreply.github.com>
2026-08-18 07:31:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/Dayflow/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
